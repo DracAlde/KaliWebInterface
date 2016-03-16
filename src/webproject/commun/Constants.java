15d48b39d6b962cbc363e68a2680e8bc5e8ca9a6
kilian <e1405101.seznec@univ-ubs.fr>
2016-03-16 16:40:13 +0100

    Remove db part (no user, no connection)

--- src/webproject/commun/Constants.java
+++ src/webproject/commun/Constants.java
@@ -9,48 +9,23 @@ package webproject.commun;
 public class Constants {
 	
 	/**
-	 * DB config constants
-	 */
-	public static final String PROPERTIES_FILE   = "/WEB-INF/config.properties";
-	public static final String PROPERTY_URL      = "url";
-	public static final String PROPERTY_DRIVER   = "driver";
-	public static final String PROPERTY_USERNAME = "username";
-	public static final String PROPERTY_PASSWORD = "password";
-    public static final String CONF_DAO_FACTORY  = "daofactory";
-	
-	/**
 	 * JSP path constants
 	 */
-	public static final String VIEW_INDEX       = "/WEB-INF/index.jsp";
-	public static final String VIEW_INSCRIPTION = "/WEB-INF/inscription.jsp";
-	public static final String VIEW_CONNECTION  = "/WEB-INF/connection.jsp";
 	public static final String VIEW_HOME        = "/WEB-INF/home.jsp";
-	public static final String VIEW_ADMIN       = "/WEB-INF/admin.jsp";
 	
 	/**
 	 * URL address constants
 	 */
-	public static final String ADDR_INDEX       = "/";
-	public static final String ADDR_INSCRIPTION = "/inscription.html";
-	public static final String ADDR_CONNECTION  = "/connection.html";
-	public static final String ADDR_HOME        = "/home.html";
-	public static final String ADDR_ADMIN       = "/admin.html";
+	public static final String ADDR_HOME        = "/";
 	
 	/**
 	 * Parameters name constants
 	 */
-	public static final String ATT_USER         = "user";
 	public static final String ATT_FORM         = "form";
-	public static final String ATT_CONNECTED    = "connected";
-	public static final String ATT_SESSION_USER = "userSession";
 	
 	/**
 	 * Form field name constants
 	 */
-	public static final String EMAIL_FIELD        = "email";
-	public static final String PASSWORD_FIELD     = "password";
-	public static final String CONFIRMATION_FIELD = "confirmation";
-	public static final String USERNAME_FIELD     = "username";
 	
 	/**
 	 * SSH session constants
