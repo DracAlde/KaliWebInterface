fd9a6f915a835e3d8665a3e1160971444430e210
arn0f <arn0f@git.com>
2016-04-12 21:44:40 +0200

    Changing Shiva logo. Trying to add system info on menu !

--- src/webproject/commun/Constants.java
+++ src/webproject/commun/Constants.java
@@ -11,33 +11,33 @@ public class Constants {
 	/**
 	 * JSP path constants
 	 */
-	public static final String VIEW_HOME         = "/WEB-INF/home.jsp";
-	public static final String VIEW_NIKTO        = "/WEB-INF/nikto.jsp";
-	public static final String VIEW_ZENMAP       = "/WEB-INF/zenmap.jsp";
-	public static final String VIEW_THEHARVESTER = "/WEB-INF/the_harvester.jsp";
-	public static final String VIEW_TLS_SLED     = "/WEB-INF/tls_sled.jsp";
-	public static final String VIEW_HYDRA        = "/WEB-INF/hydra.jsp";
-	public static final String VIEW_DNS_RECON    = "/WEB-INF/dns_recon.jsp";
+	public static final String VIEW_HOME         	= "/WEB-INF/home.jsp";
+	public static final String VIEW_NIKTO        	= "/WEB-INF/nikto.jsp";
+	public static final String VIEW_ZENMAP       	= "/WEB-INF/zenmap.jsp";
+	public static final String VIEW_THEHARVESTER 	= "/WEB-INF/the_harvester.jsp";
+	public static final String VIEW_TLS_SLED     	= "/WEB-INF/tls_sled.jsp";
+	public static final String VIEW_HYDRA        	= "/WEB-INF/hydra.jsp";
+	public static final String VIEW_DNS_RECON    	= "/WEB-INF/dns_recon.jsp";
 	
 	/**
 	 * Language constants
 	 */
-	public String LANGUAGE						= "EN";
-	public static final String LANG_PATH_FR 	= "/WebContent/lang/fr.xml";
-	public static final String LANG_PATH_EN		= "/WebContent/lang/en.xml";
+	public String LANGUAGE							= "EN";
+	public static final String LANG_PATH_FR 		= "/WebContent/lang/fr.xml";
+	public static final String LANG_PATH_EN			= "/WebContent/lang/en.xml";
 	/* TO BE REPLACED IN THE FUTURE */
-	public static final String MY_PROJECT_PATH	= "/home/arn0f/Modèles/Workspace/Eclipse JEE/git/KaliWebInterface";
+	public static final String MY_PROJECT_PATH		= "/home/arn0f/Modèles/Workspace/Eclipse JEE/git/KaliWebInterface";
 	
 	/**
 	 * URL address constants
 	 */
-	public static final String ADDR_HOME         = "/";
-	public static final String ADDR_NIKTO        = "/nikto.html";
-	public static final String ADDR_ZENMAP       = "/zenmap.html";
-	public static final String ADDR_THEHARVESTER = "/theharvester.html";
-	public static final String ADDR_TLS_SLED     = "/tlssled.html";
-	public static final String ADDR_HYDRA        = "/hydra.html";
-	public static final String ADDR_DNS_RECON    = "/dnsrecon.html";
+	public static final String ADDR_HOME         	= "/";
+	public static final String ADDR_NIKTO        	= "/nikto.html";
+	public static final String ADDR_ZENMAP       	= "/zenmap.html";
+	public static final String ADDR_THEHARVESTER 	= "/theharvester.html";
+	public static final String ADDR_TLS_SLED     	= "/tlssled.html";
+	public static final String ADDR_HYDRA        	= "/hydra.html";
+	public static final String ADDR_DNS_RECON    	= "/dnsrecon.html";
 	
 	/**
 	 * Parameters name constants
@@ -49,15 +49,25 @@ public class Constants {
 	/**
 	 * Form field name constants
 	 */
-	public static final String FIELD_HOSTNAME = "hostname";
-	public static final String FIELD_PORT = "port";
+	public static final String FIELD_HOSTNAME 		= "hostname";
+	public static final String FIELD_PORT 			= "port";
 	
 	/**
 	 * SSH session constants
 	 */
-	public static final String SSH_USERNAME = "username";
-	public static final String SSH_HOST     = "127.0.0.1";
-	public static final String SSH_PASSWORD = "password";
+	public static final String SSH_USERNAME 		= "username";
+	public static final String SSH_HOST     		= "127.0.0.1";
+	public static final String SSH_PASSWORD 		= "password";
+	
+	/**
+	 * Tool name used in Languages file ("*.xml")
+	 */
+	public static final String TOOL_NIKTO			= "nikto";
+	public static final String TOOL_ZENMAP			= "zenmap";
+	public static final String TOOL_THE_HARVESTER	= "the-harvester";
+	public static final String TOOL_TLS_SLED		= "tls-sled";
+	public static final String TOOL_HYDRA			= "hydra";
+	public static final String TOOL_DNS_RECON		= "dns-recon";
 	
 	
 	
